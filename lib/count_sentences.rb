e285ee24d3be186a10b9e337eea456dc30e29b5d
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-22 02:05:55 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,10 +3,11 @@ require 'pry'
 class String
 
   def sentence?
-    if sentence.end_with?(".")
-      puts true
-    elsif sentence.end_with?(" ")
-      puts false
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
